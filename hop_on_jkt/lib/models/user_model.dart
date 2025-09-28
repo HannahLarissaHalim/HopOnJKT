972cb542f8ccba482be24bc3ddae1fc985644b95
hannahlrhl <hannahlarissahalim@gmail.com>
2025-09-29 01:43:09 +0700

    add user_model.dart

---
+++ hop_on_jkt/lib/models/user_model.dart
@@ -0,0 +1,38 @@
+// class ini representasi data user di aplikasi
+// selain data dr firebase auth, ada jg data tambahan yang kita simpan di firestore
+// setiap signup, bikin object UserModel, lalu disimpan di firestore
+
+class UserModel {
+  final String uid;
+  final String email;
+  final int points;
+  final String pin;
+
+  // constructor
+  UserModel({
+    required this.uid,
+    required this.email,
+    required this.points,
+    required this.pin,
+  });
+
+  // convert object jd map
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'points': points,
+      'pin': pin,
+    };
+  }
+
+  // factory constructor buat bikin object dari data firestore
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      uid: map['uid'] ?? '',
+      email: map['email'] ?? '',
+      points: map['points'] ?? 0,
+      pin: map['pin'] ?? '',
+    );
+  }
+}
