a7a31ae53b34657129d8955322a095c95cf0c230
hannahlrhl <hannahlarissahalim@gmail.com>
2025-10-02 19:26:09 +0700

    Revert "update ticket_tab.dart, ganti dummy data BuyTicketScreen dengan RouteScreen dan pakai userId dr firebase"
    This reverts commit 4ceddefd5bda61bccd8945dbc9c32a7bf743b69d.

--- hop_on_jkt/lib/screens/ticket/ticket_tab.dart
+++ hop_on_jkt/lib/screens/ticket/ticket_tab.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'buy_ticket_screen.dart';
 import 'order_history_screen.dart';
-import '../journey/route_screen.dart';
 
 class TicketTab extends StatelessWidget {
   const TicketTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser; // ✅ ambil user yg login
-    final userId = user?.uid ?? ""; // kalau null = kosong
-
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -23,17 +19,15 @@ class TicketTab extends StatelessWidget {
             ],
           ),
         ),
-        body: TabBarView(
+        body: const TabBarView(
           children: [
-            RouteScreen(
-              // user pilih rute dulu
-              from: "Manggarai",
-              to: "Jakarta Kota",
-              date: DateTime.now(),
-              departureTime: DateTime.now(),
+            BuyTicketScreen(
+              fromStation: "Jakarta",
+              toStation: "Bandung",
+              price: 120,
             ),
             OrderHistoryScreen(
-              userId: userId, // ✅ pakai user login
+              userId: "dummyUser123", 
             ),
           ],
         ),
