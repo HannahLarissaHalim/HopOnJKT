22b113610e674882d759563b4f1259fd21275348
hannahlrhl <hannahlarissahalim@gmail.com>
2025-10-02 18:22:01 +0700

    update bottom_navbar.dart, perbaiki navigasi bottom navbar, hilangkan dummy userId dan pakai userId login

--- hop_on_jkt/lib/widgets/bottom_navbar.dart
+++ hop_on_jkt/lib/widgets/bottom_navbar.dart
@@ -4,7 +4,7 @@ import '../screens/journey/route_screen.dart';
 import '../screens/ticket/order_history_screen.dart';
 
 class BottomNavBar extends StatefulWidget {
-  final int initialIndex; //  untuk masuk langsung ke tab tertentu
+  final int initialIndex; // untuk masuk langsung ke tab tertentu
   const BottomNavBar({super.key, this.initialIndex = 0}); 
 
   @override
@@ -17,32 +17,30 @@ class _BottomNavBarState extends State<BottomNavBar> {
   @override
   void initState() {
     super.initState();
-    _currentIndex = widget.initialIndex; //supaya bisa start dari tab ke-2
+    _currentIndex = widget.initialIndex; // supaya bisa start dari tab ke-2
   }
-
-  final List<Widget> _screens = [
-    RouteScreen(from: "Manggarai", to: "Jakarta Kota", date: DateTime.now()),
-    OrderHistoryScreen(userId: "dummyUserId"), 
-    Center(child: Text("Account Page (soon)")),
-  ];
 
   @override
   Widget build(BuildContext context) {
-
     // ambil userId dari FirebaseAuth
     final userId = FirebaseAuth.instance.currentUser?.uid ?? "";
 
     // masukkan userId ke OrderHistoryScreen
-    final List<Widget> _screens = [
-      RouteScreen(from: "Manggarai", to: "Jakarta Kota", date: DateTime.now()),
-      OrderHistoryScreen(userId: userId),
+    final List<Widget> screens = [
+      RouteScreen(
+        from: "Manggarai",
+        to: "Jakarta Kota",
+        date: DateTime.now(),
+        departureTime: DateTime.now(), // ✅ tambahkan biar gak error
+      ),
+      OrderHistoryScreen(userId: userId), 
       const Center(child: Text("Account Page (soon)")),
     ];
 
     return Scaffold(
       body: IndexedStack( // supaya state tiap screen ga reset
         index: _currentIndex,
-        children: _screens,
+        children: screens,
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
