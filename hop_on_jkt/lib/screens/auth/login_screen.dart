c71d977f8090987ead092871a2aee3a3e6156446
hannahlrhl <hannahlarissahalim@gmail.com>
2025-10-04 20:15:27 +0700

    update snackbar message lagi

--- hop_on_jkt/lib/screens/auth/login_screen.dart
+++ hop_on_jkt/lib/screens/auth/login_screen.dart
@@ -153,7 +153,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                 );
                                 ScaffoldMessenger.of(context).showSnackBar(
                                   const SnackBar(
-                                    content: Text("Login successful! :D"),
+                                    content: Text("Logged in successfully! :D"),
                                     backgroundColor: Colors.green,
                                   ),
                                 );
